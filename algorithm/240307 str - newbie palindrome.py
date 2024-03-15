2f183c7258dd058745ab2da2d3598308ca67978e
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-03-15 10:51:26 +0900

    문제 풀이 완료

--- algorithm/240307 str - newbie palindrome.py
+++ algorithm/240307 str - newbie palindrome.py
@@ -1,24 +1,23 @@
 # 초심자의 회문 검사
 # 회문이면 1 출력, 아니라면 0 출력
 # 예제 input
-# 3
-# level
-# samsung
-# eye
+
+
 
 t = int(input())
 result = []
 
 for i in range(1, t+1):
     temp = input()
-    if temp == temp[::-1]:
+    if temp == temp[::-1]: # 회문일 때
         print(f'#{i} 1')
-    else:
+    else: # 회문이 아닐 때
         print(f'#{i} 0')
 
 
-    # if temp == temp[::-1]:
-    #     print('palindrome')
-    # else:
-    #     print('not palindrome')
-
+''' input
+3
+level
+samsung
+eye
+'''
