875be1f1119fe8e5a2317f3fe0103dc96a47d091
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-04-04 15:33:49 +0900

    댓글 작성, 삭제 기능까지 구현

--- TeamProject2/app.py
+++ TeamProject2/app.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
 from flask_sqlalchemy import SQLAlchemy
 
@@ -43,7 +43,21 @@ with app.app_context():
 @app.route("/")
 def home():
     comment_list = Comment.query.all()
-    return render_template("test.html", data=comment_list)
+    if comment_list:
+        username = comment_list[0].username
+    else:
+        username = None
+    return render_template("test.html", data=comment_list, username=username)
+
+# @app.route("/")
+# def home():
+#     try:
+#         comment_list = Comment.query.all()
+#         print(comment_list)  # Check the contents of comment_list in the console
+#         return render_template("test.html", data=comment_list)
+#     except Exception as e:
+#         print(f"Error fetching comments: {e}")
+#         return "Error fetching comments"
 
 
 # @app.route("/write")
@@ -60,18 +74,36 @@ def home():
 #     db.session.commit()
 #     return redirect(url_for(''))
 
-@app.route("/test")
+# 댓글 작성
+@app.route("/comments/create", methods=['GET', 'POST'])
 def write_comment():
     # 데이터 받아오기
-    username_comment = request.args.get("username")
-    contents_comment = request.args.get("contents")
+    username_comment = request.form.get("username")
+    contents_comment = request.form.get("contents")
 
-    # 데이터 db에 저장
-    comment = Comment(username=username_comment, contents=contents_comment)
-    db.session.add(comment)
-    db.session.commit()
+    if request.method == 'POST':
+        if Comment.query.filter_by(username=username_comment).first():
+            comment = Comment.query.filter_by(
+                username=username_comment).first()
+        else:
+            new_comment = Comment(username=username_comment,
+                                  contents=contents_comment)
+            db.session.add(new_comment)
+        db.session.commit()
     return redirect(url_for('home'))
 
+
+# # 댓글 조회
+# @app.route("/comments", methods=['GET'])
+# def get_comments():
+#     comments = Comment.query.all()
+#     return jsonify([{'id': comments.id, 'username': comments.username, 'contents': comments.contents}])
+
+
+# @app.route("/comments/<int:id>", methods=['GET'])
+# def display_comments(id):
+#     comment = Comment.query.get_or_404(id)
+#     return render_template('test.html', comment=comment)
 
 # @app.route("/test/edit", methods=['PUT'])
 # def edit_comment():
@@ -81,31 +113,69 @@ def write_comment():
 #     comment.contents = 'new_contents'
 #     db.session.commit()
 
-@app.route("/test/edit", methods=['POST'])
-def edit_comment():
-    updated_username = request.form.get("new_username")
-    updated_contents = request.form.get("new_contents")
-    original_contents = request.form.get("contents")
+# @app.route("/test/edit", methods=['POST'])
+# def edit_comment():
+#     original_id = request.form.get("id")
+#     # updated_username = request.form.get("new_username")
+#     # updated_contents = request.form.get("new_contents")
+#     comment = Comment.query.filter_by(contents=original_id).first()
+#     print(original_id)
+#     print(comment)
+#     if comment:
+#         print(comment)
+#         # db.session.delete(comment)
+#         # comment = Comment(username=updated_username, contents=updated_contents)
+#         comment.username = updated_username
+#         comment.contents = updated_contents
+#         # db.session.add(comment)
+#         db.session.commit()
 
-    comment = Comment.query.filter_by(contents=original_contents).first()
+#     return redirect(url_for('home'))
 
+@app.route("/comments/<username>/edit-modal")
+def edit_comment_modal(username):
+    comment = Comment.query.filter_by(username=username).first()
     if comment:
-        comment.username = updated_username
-        comment.contents = updated_contents
-        db.session.commit()
-        message = "댓글이 수정되었습니다."
-    return redirect(url_for('home', message=message))
+        return render_template("edit_comment.html", comment=comment)
+    else:
+        # Handle the case where the comment does not exist
+        return "Comment not found", 404
 
 
-@app.route("/test/delete", methods=['POST'])
-def delete_comment():
-    contents = request.form.get("contents")
-    comment = Comment.query.filter_by(contents=contents).first()
+@app.route("/comments/<username>/edit", methods=['GET', 'POST'])
+def edit_comment(username):
+    if request.method == 'POST':
+        comment_id = request.form.get("comment_id")
+        updated_username = request.form.get("new_username")
+        updated_contents = request.form.get("new_contents")
+
+        comment = Comment.query.filter_by(id=comment_id).first()
+        if comment:
+            # db.session.delete(comment)
+            # comment = Comment(username=updated_username, contents=updated_contents)
+            comment.username = updated_username
+            comment.contents = updated_contents
+            # db.session.add(comment)
+            db.session.commit()
+
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('home'))
+
+
+# @app.route("/test/edit/<int:comment_id>")
+# def edit_comment_modal(comment_id):
+#     comment = Comment.query.get(comment_id)
+#     return render_template("edit_comment.html", comment=comment)
+
+
+@app.route("/comments/<username>/delete", methods=['POST'])
+def delete_comment(username):
+    comment = Comment.query.filter_by(username=username).first()
     if comment:
         db.session.delete(comment)
         db.session.commit()
-    message = "댓글이 삭제되었습니다."
-    return redirect(url_for('home', message=message))
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
